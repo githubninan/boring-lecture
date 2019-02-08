845c375a18127960d92aec2a23c787afa11ee7c6
nn <ninain@gmail.com>
2019-02-08 11:45:35 -0800

    add jquery bootstrap files, add highlighting property to the p element

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#yellow-hglt").click(function() {
+    $("#yellow-hglt").removeClass();
+    $("#yellow-hglt").addClass("yellow-hglt");
+  });
+
+  $("#green-hglt").click(function() {
+    $("#green-hglt").removeClass();
+    $("#green-hglt").addClass("green-hglt");
+  });
+
+  $("#red-hglt").click(function() {
+    $("#red-hglt").removeClass();
+    $("#red-hglt").addClass("red-hglt");
+  });
+});
